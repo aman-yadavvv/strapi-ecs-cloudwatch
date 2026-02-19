79fb8f9e525d9441c08ef447784b9888999f04c5
Aman Yadav <69920693+aman-yadavvv@users.noreply.github.com>
2026-02-19 17:17:32 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,12 @@ output "app_url" {
 
 output "ecr_repository_url" {
   description = "ECR repository URL"
-  value       = aws_ecr_repository.main.repository_url
+  value       = data.aws_ecr_repository.main.repository_url
 }
 
 output "cloudwatch_log_group" {
   description = "CloudWatch log group"
-  value       = aws_cloudwatch_log_group.ecs.name
+  value       = data.aws_cloudwatch_log_group.ecs.name
 }
 
 output "cloudwatch_dashboard" {
